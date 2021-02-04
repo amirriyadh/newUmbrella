8af6d95b41af5b73752974065ab0b32aee63de68
amirriyadh <amir.whiz@gmail.com>
2021-02-05 00:12:06 +0300

    Initial Commit

---
+++ newUmbrella/newUmbrella.h
@@ -0,0 +1,18 @@
+//
+//  newUmbrella.h
+//  newUmbrella
+//
+//  Created by Amir Riyadh on 05/02/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for newUmbrella.
+FOUNDATION_EXPORT double newUmbrellaVersionNumber;
+
+//! Project version string for newUmbrella.
+FOUNDATION_EXPORT const unsigned char newUmbrellaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <newUmbrella/PublicHeader.h>
+
+
